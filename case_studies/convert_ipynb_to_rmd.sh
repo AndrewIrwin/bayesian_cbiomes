b86fe5fb3d85a31df0133af17f90be46725b77e6
Andrew Irwin <a.irwin@dal.ca>
2020-01-08 10:00:10 -0500

    more rmd conversions

--- case_studies/convert_ipynb_to_rmd.sh
+++ case_studies/convert_ipynb_to_rmd.sh
@@ -1,6 +1,13 @@
+# convert R.ipynb to .rmd
+# a few steps:
+# - remove output from jupyter notebook
+# - convert jupyter notebook to markdown
+# - fix two translation "errors": ```R becomes ```{r} and open_progress=FALSE
+# - rename file
+# - cleanup
 FILE=$1
 ./remove_output.py -o temp.ipynb $1
 jupyter nbconvert --to markdown temp.ipynb
 NEWFILE=${FILE%ipynb}rmd
-sed -e 's/```R/```{r}/' -e 's/open_progress=FALSE/open_progress=FALSE/' < temp.md > $NEWFILE
+sed -e 's/```R/```{r}/' -e 's/open_progress=TRUE/open_progress=FALSE/' < temp.md > $NEWFILE
 rm temp.ipynb temp.md
